e5c23b2fc020b20ae7334958443dbe958e02bd11
Quan Zhou <zhouquan@iscas.ac.cn>
2024-06-25 17:57:10 +0800

    add x86

---
+++ x86/start-linux-host.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+qemu-system-x86_64 \
+	-enable-kvm \
+	-kernel $LINUX/build/arch/x86/boot/bzImage \
+	-boot c -m 2048M \
+	-hda $BUILDROOT/output/images/rootfs.ext4 \
+	-append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
+	-serial stdio -display none
+
