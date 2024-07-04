2f60bb02599e637e933edc525a3a880db3c3ab0f
Quan Zhou <zhouquan@iscas.ac.cn>
2024-07-05 01:15:28 +0800

    update start-linux-host-rv64-fast.sh to common env-var

--- start-linux-host-rv64-fast.sh
+++ start-linux-host-rv64-fast.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-$QEMU/build/qemu-system-riscv64 \
+$QEMU/qemu-system-riscv64 \
     -M virt \
     -cpu rv64,v=true \
     -m 4G -nographic \
     -smp 8 \
     -kernel $LINUX/Image \
     -append "root=/dev/vda rw console=ttyS0 loglevel=3 earlycon=sbi" \
-    -drive file=./rootfs.ext4,format=raw,id=hd0,cache=writeback \
+    -drive file=$ROOTFS/rootfs.ext4,format=raw,id=hd0,cache=writeback \
     -device virtio-blk-pci,drive=hd0 \
     -netdev user,id=usernet,hostfwd=tcp:127.0.0.1:7722-0.0.0.0:22 \
     -device virtio-net-pci,netdev=usernet \
