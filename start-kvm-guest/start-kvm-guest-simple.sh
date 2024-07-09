485d62f7bc17aea8b839a91f9b16b28c5c7cff8c
Quan Zhou <zhouquan@iscas.ac.cn>
2024-07-10 00:43:08 +0800

    add a start-kvm-guest.sh with -virtfs option

---
+++ start-kvm-guest/start-kvm-guest-simple.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$QEMU/qemu-system-riscv64 \
+	-cpu rv64 -M virt \
+	-m 512M -nographic \
+	-kernel $LINUX/Image \
+	-initrd $ROOTFS/rootfs_kvm_riscv64.img \
+	-virtfs local,path=/root/repo/shared,mount_tag=host0,security_model=passthrough,id=host0 \
+	-append "root=/dev/ram rw console=ttyS0 earlycon=sbi"
